79745e66d7ca15506d9a6874900b06317bb7e50b
Travis Clarke <travis.m.clarke@gmail.com>
2017-12-01 13:56:22 -0800

    leverage exec system call to avoid issues attaching tty

--- main.go
+++ main.go
@@ -3,42 +3,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"runtime"
+	"syscall"
 )
 
 func main() {
 	_, callPath, _, _ := runtime.Caller(0)
-	scriptPath := fmt.Sprintf("%s/%s-%s/docker-wrapper", filepath.Dir(callPath), runtime.GOOS, runtime.GOARCH)
-	cmd := exec.Command(scriptPath, os.Args[1:]...)
-	stdout, _ := cmd.StdoutPipe()
-	stderr, _ := cmd.StderrPipe()
-	cmd.Start()
-	ch := make(chan string, 100)
-	stdoutScan := bufio.NewScanner(stdout)
-	stderrScan := bufio.NewScanner(stderr)
-
-	go func() {
-		for stdoutScan.Scan() {
-			line := stdoutScan.Text()
-			ch <- line
-		}
-	}()
-	go func() {
-		for stderrScan.Scan() {
-			line := stderrScan.Text()
-			ch <- line
-		}
-	}()
-	go func() {
-		cmd.Wait()
-		close(ch)
-	}()
-	for line := range ch {
-		fmt.Println(line)
-	}
+	binary := fmt.Sprintf("%s/%s-%s/docker-wrapper", filepath.Dir(callPath), runtime.GOOS, runtime.GOARCH)
+	env := os.Environ()
+	syscall.Exec(binary, os.Args, env)
 }
